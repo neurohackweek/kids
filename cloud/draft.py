898fed44967cf54e94530e70023f410581d14c5e
ccarey <cemcarey@gmail.com>
2016-09-07 15:45:50 -0500

    removed site_id from keys

--- cloud/draft.py
+++ cloud/draft.py
@@ -6,21 +6,23 @@ def split_data(df, keys, num_iter=3, random_state=0):
     labels = [ '.'.join([str(df[key][i]) for key in keys]) for i in range(len(df))]
     return StratifiedShuffleSplit(labels, num_iter, test_size=0.5, random_state=random_state)
 
+filename = 'Phenotypic_V1_0b_preprocessed1.csv'
+
 csv = pd.read_csv(filename)
 
-keys = ['AGE_AT_SCAN', 'SITE_ID', 'SEX' ]
+keys = ['AGE_AT_SCAN', 'SEX']
 
 # age -> 0:young/1:old (median)
 for key in keys:
     if 'AGE' in key:    # TODO: better way?
     #if len(df[key].unique())>2:
-            df[key] = pd.qcut(df[key], 2, labels=[0,1])
+            csv[key] = pd.qcut(csv[key], 2, labels=[0,1])
 
 sss = split_data(csv, keys, num_iter=3, random_state=0)
 print sss
 assess = []
 for index_a, index_b in sss:
-    #print("TRAIN:", train_index, "TEST:", test_index)
+    #print("TRAIN:", index_a, "TEST:", index_b)
     set_a, set_b = csv.iloc[index_a], csv.iloc[index_b]
     # image data[index_a/b]
     
