32870ca5c860f9086aacad05e2efab77276ab365
ccarey <cemcarey@gmail.com>
2016-09-07 01:37:38 -0500

    added median split of continuous input

--- cloud/draft.py
+++ cloud/draft.py
@@ -1,14 +1,18 @@
+import pandas as pd
+from sklearn.cross_validation import StratifiedShuffleSplit
+import numpy as np
 
-def split_data(df, keys, num_iter=3, test_size=0.5, random_state=0):
-    from sklearn.cross_validation import StratifiedShuffleSplit
+def split_data(df, keys, num_iter=3, random_state=0):
+    output = df[keys].apply(lambda x: len(x.unique()))
+    for key in keys:
+	if len(df[key].unique())>2:
+		df[key] = pd.qcut(df[key], 2, labels=[0,1])
     labels = [ '.'.join([str(df[key][i]) for key in keys]) for i in range(len(df))]
     return StratifiedShuffleSplit(labels, num_iter, test_size=0.5, random_state=0)
-    
-
-import pandas as pd
 
 csv = pd.read_csv(filename)
-sss = split_data(csv, keys, num_iter=3, test_size=0.5, random_state=0)
+sss = split_data(csv, keys, num_iter=3, random_state=0)
+print sss
 assess = []
 for index_a, index_b in sss:
     #print("TRAIN:", train_index, "TEST:", test_index)
