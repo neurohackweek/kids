a5bd327cb890f6a9100f37e16640a3d6f920b3f8
Cameron Craddock <cameorn.craddock@gmail.com>
2016-09-09 22:01:46 +0000

    minor updates and qsub loop

--- loop_abide_motion_qsub_v2.sh
+++ loop_abide_motion_qsub_v2.sh
@@ -7,7 +7,7 @@ for motion_thresh in {2..40..2}; do
                 age_u=$(($age_l + $age_gap))
                 if [[ ${age_u} -lt 19 ]]; then 
                      echo "Running: motion_thresh: ${motion_thresh}, age range ${age_l} to ${age_u}, sample size ${n}"
-                    ./SgeAbideMotion.sh ${motion_thresh} ${age_l} ${age_u} ${n} 10 0
+                    qsub ./SgeAbideMotion.sh ${motion_thresh} ${age_l} ${age_u} ${n} 10 0
                 fi
             done
         done
