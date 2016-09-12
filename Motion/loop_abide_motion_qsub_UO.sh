029c3fd7fc55f17968ccab9f7fb7c3b4ea6918af
John Flournoy <flournoy@hn1.(none)>
2016-09-12 14:17:34 -0700

    edits

--- Motion/loop_abide_motion_qsub_UO.sh
+++ Motion/loop_abide_motion_qsub_UO.sh
@@ -7,7 +7,7 @@ for motion_thresh in {2..50..2}; do
                 age_u=$(($age_l + $age_gap))
                 if [[ ${age_u} -lt 19 ]]; then 
                     echo "Running: motion_thresh: ${motion_thresh}, age range ${age_l} to ${age_u}, sample size ${n}"
-                    qsub -q short -l nodes=1:ppn=1 -v motion_thresh=${motion_thresh},age_l=${age_l},age_u=${age_u},n=${n},n_perms=100,overwrite=1 SgeAbideMotion_UO.sh
+                    qsub -d ~/kids/Motion/ -e ~/ -o ~/ -q short -l nodes=1:ppn=1 -v motion_thresh=${motion_thresh},age_l=${age_l},age_u=${age_u},n=${n},n_perms=100,overwrite=1 SgeAbideMotion_UO.sh
                 fi
             done
         done
