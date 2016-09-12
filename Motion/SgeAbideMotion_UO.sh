80ecc453ddb2e405154dcf281b85941be293e4a4
John Flournoy <flournoy@hn1.(none)>
2016-09-12 14:17:21 -0700

    eddit

--- Motion/SgeAbideMotion_UO.sh
+++ Motion/SgeAbideMotion_UO.sh
@@ -6,8 +6,8 @@
 # srun SgeAbideMotion.sh motion_thresh age_l age_u n n_perms
 #########################################################################
 # Defines path to the python code you want to run 
-Code="~/kids/Motion/abide_motion_wrapper.py"
+Code="/home5/flournoy/kids/Motion/abide_motion_wrapper.py"
 # Defines motion threshold from input 1
 echo Running $Code
 date
-python $Code $motion_thresh $age_l $age_u $n $n_perms $overwrite
+/home5/flournoy/miniconda2/envs/kids/bin/python $Code $motion_thresh $age_l $age_u $n $n_perms $overwrite
