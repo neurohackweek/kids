731415d2f8ab7af41bb2969859570a45097c42e0
John Flournoy <flournoy@hn1.(none)>
2016-09-13 09:07:52 -0700

    environment passing

--- Motion/loop_abide_motion_qsub_array_UO.sh
+++ Motion/loop_abide_motion_qsub_array_UO.sh
@@ -9,13 +9,15 @@ PARAMFILE=abide_motion_parameters.tsv
 
 line=$(cat $PARAMFILE | head -n $PBS_ARRAYID | tail -n 1)
 
-motion_thresh=$(echo "$line" | cut -f1)
-age_l=$(echo "$line" | cut -f2)
-age_u=$(echo "$line" | cut -f3)
-n=$(echo "$line" | cut -f4)
-n_perms=$(echo "$line" | cut -f5)
-overwrite=$(echo "$line" | cut -f6)
+export motion_thresh=$(echo "$line" | cut -f1)
+export age_l=$(echo "$line" | cut -f2)
+export age_u=$(echo "$line" | cut -f3)
+export n=$(echo "$line" | cut -f4)
+export n_perms=$(echo "$line" | cut -f5)
+export overwrite=$(echo "$line" | cut -f6)
 
-#PBS -v motion_thresh=${motion_thresh},age_l=${age_l},age_u=${age_u},n=${n},n_perms=100,overwrite=1
+#motion_thresh=${motion_thresh},age_l=${age_l},age_u=${age_u},n=${n},n_perms=100,overwrite=1
+
+#PBS -V 
 
 /home5/flournoy/kids/Motion/SgeAbideMotion_UO.sh
