5ced9fe8a3dfe230bb4e08470951399c3975585e
jlnrd <jlnrd@mit.edu>
2016-09-22 10:30:43 -0400

    adding bin sampling functionality

--- Motion/create_qsub_variable_array_UO.sh
+++ Motion/create_qsub_variable_array_UO.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-for motion_thresh in {5..50..5}; do
-    for n in {30..300..30}; do 
-        for age_l in {6..16}; do 
-            for age_gap in 3; do 
-                age_u=$(($age_l + $age_gap))
-                if [[ ${age_u} -lt 19 ]]; then 
-                    echo "${motion_thresh}	${age_l}	${age_u}	${n}	100	1"
-                fi
-            done
-        done
+for motion_thresh in {5..80..5}; do
+    for n in {10..100..10}; do 
+        echo "${motion_thresh}	6	18	${n}	100	1"
+    fi
     done
 done > abide_motion_parameters.tsv
