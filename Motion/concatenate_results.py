b62be00a111029daf6f875845eb540caad233321
John Flournoy <uoflournoy@gmail.com>
2016-09-23 15:09:29 -0700

    switched dir

--- Motion/concatenate_results.py
+++ Motion/concatenate_results.py
@@ -4,8 +4,8 @@ from glob import glob
 columns = [ 'motion_thresh', 'med_rsq', 'CI_95', 'med_icc', 'CI_95_icc', 'n', 'age_l', 'age_u' ]
 results_df = pd.DataFrame(columns = columns)
 
-for f in glob('RESULTS/*csv'):
+for f in glob('RESULTS_bin/*csv'):
    temp_df = pd.read_csv(f, index_col=0)
    results_df = results_df.append(temp_df)
    
-results_df.to_csv('RESULTS/SummaryRsqs.csv', index=None, columns=columns)
+results_df.to_csv('RESULTS_bin/SummaryRsqs.csv', index=None, columns=columns)
