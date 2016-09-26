450c2626b38751b33507de8290925dd5edcf9d6e
Julia Leonard <jlnrd@openmind7.eth.cluster>
2016-09-26 14:48:28 -0400

    updated running scripts

--- Motion/loop_abide_motion_qsub_MIT.sh
+++ Motion/loop_abide_motion_qsub_MIT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for motion_thresh in {5..80..5}; do
-    for n in {10..100..10}; do 
+for motion_thresh in {10..50..10}; do
+    for n in {10..20..10}; do 
     	echo "Running: motion_thresh: ${motion_thresh}, age range 6 to 18, sample size ${n}"
         sbatch SgeAbideMotion_MIT.sh ${motion_thresh} 6 18 ${n} 100 1
     done
